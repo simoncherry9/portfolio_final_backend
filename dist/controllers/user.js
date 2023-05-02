371df106e6dd7dc02ba2d986037fb565b7053a1f
Simon <116177578+simoncherry9@users.noreply.github.com>
2023-05-02 07:52:23 -0300

    Integrando jwt al proyecto

--- dist/controllers/user.js
+++ dist/controllers/user.js
@@ -15,6 +15,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.loginUser = exports.newUser = void 0;
 const bcrypt_1 = __importDefault(require("bcrypt"));
 const user_1 = require("../models/user");
+const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
 const newUser = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const { username, password } = req.body;
     // Validamos si el usuario ya existe en la base de datos
@@ -43,11 +44,26 @@ const newUser = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     }
 });
 exports.newUser = newUser;
-const loginUser = (req, res) => {
-    const { body } = req;
-    res.json({
-        msg: "Login user",
-        body
-    });
-};
+const loginUser = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { username, password } = req.body;
+    // Validamos si el usuario existe en la base de datos
+    const user = yield user_1.User.findOne({ where: { username: username } });
+    if (!user) {
+        return res.status(400).json({
+            msg: "No existe un usuario con el nombre " + username + " en la base de datos",
+        });
+    }
+    // Validamos password
+    const passwordValid = yield bcrypt_1.default.compare(password, user.password);
+    if (!passwordValid) {
+        return res.status(400).json({
+            msg: "Password incorrecto"
+        });
+    }
+    // Generamos token
+    const token = jsonwebtoken_1.default.sign({
+        username: username
+    }, process.env.SECRET_KEY || 'pepito123');
+    res.json(token);
+});
 exports.loginUser = loginUser;
