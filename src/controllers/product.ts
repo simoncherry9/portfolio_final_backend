f95fd605a8527aed0a373c51d05a0f220e10d4c2
Simon <116177578+simoncherry9@users.noreply.github.com>
2023-05-06 09:51:22 -0300

    Eliminacion de ficheros de prueba

--- src/controllers/product.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from 'express';
-import { Product } from '../models/product';
-
-export const getProducts = async (req: Request, res: Response) => {
-    const listProducts = await Product.findAll();
-    
-    res.json(listProducts)
-
-}
